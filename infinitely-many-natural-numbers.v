158bd6d3c6d61330e42af4a1ec30f0703f46db92
AurelienAlvarez <AurelienAlvarez@users.noreply.github.com>
2018-04-20 15:04:02 +0200

    Update infinitely-many-natural-numbers.v

--- infinitely-many-natural-numbers.v
+++ infinitely-many-natural-numbers.v
@@ -15,10 +15,10 @@ Fixpoint sum (l : list nat) : nat :=
     | x::l' => x + sum l'
   end.
 
-Lemma le_sum : forall l : list nat, forall x : nat, In x l -> x <= sum l.
+Lemma sum_le : forall l : list nat, forall x : nat, In x l -> x <= sum l.
 Proof.
   intros l x Hin. induction l ; destruct Hin ; simpl.
-  rewrite H. omega. assert (foo := IHl H). omega.
+  rewrite H. omega. apply IHl in H. omega.
 Qed.
 
 
@@ -32,11 +32,11 @@ Fixpoint maximum (l : list nat) : nat :=
     | x::l' => max x (maximum l')
   end.
 
-Lemma le_max : forall l : list nat, forall x : nat, In x l -> x <= maximum l.
+Lemma maximum_le : forall l : list nat, forall x : nat, In x l -> x <= maximum l.
 Proof.
   intros l x Hin. induction l ; destruct Hin.
   rewrite <- H ; simpl ; apply Max.le_max_l. simpl.
-  assert (trans1 := IHl H). assert (trans2 := Max.le_max_r a (maximum l)). omega.
+  apply IHl in H. assert (trans := Max.le_max_r a (maximum l)). omega.
 Qed.
 
 
@@ -52,12 +52,10 @@ Fixpoint product (l : list nat) : nat :=
 
 Lemma product_lt_0 : forall l : list nat, product l > 0.
 Proof.
-  intro l. induction l.
-    simpl ; auto.
-    destruct a.
-      simpl ; exact IHl.
-      simpl. apply lt_le_trans with (n := 0) (m := product l) (p := product l + a * product l).
-      exact IHl. auto with arith.
+  intro l. induction l ; simpl ; auto.
+  destruct a. exact IHl. simpl.
+  apply lt_le_trans with (n := 0) (m := product l) (p := product l + a * product l).
+  exact IHl. auto with arith.
 Qed.
 
 Corollary product_lt_x : forall l : list nat, forall x : nat, x <= x * product l.
@@ -79,18 +77,18 @@ Proof.
   rewrite <- (Nat.mul_0_l 1). apply Nat.mul_le_mono ; [omega | apply product_lt_0].
 Qed.
 
-Corollary product_le : forall l : list nat, forall x : nat, max x (product l) <= product (x::l).
+Corollary product_max : forall l : list nat, forall x : nat, max x (product l) <= product (x::l).
 Proof.
   intros l x. apply Nat.max_lub ; [apply product_le_x | apply product_le_l].
 Qed.
 
-Lemma le_prod : forall l : list nat, forall x : nat, In x l -> x <= product l.
+Lemma product_le : forall l : list nat, forall x : nat, In x l -> x <= product l.
 Proof.
   intros l x Hin. induction l. elim Hin. destruct x.
   exact (lt_le_weak 0 (product (a::l)) (product_lt_0 (a::l))). destruct Hin.
-  rewrite H. apply Nat.max_lub_l with (n := S x) (m := product l) (p := product (S x::l)) ; exact (product_le l (S x)).
+  rewrite H. apply Nat.max_lub_l with (n := S x) (m := product l) (p := product (S x::l)) ; exact (product_max l (S x)).
   apply le_trans with (n := S x) (m := product l) (p := product (a::l)). exact (IHl H).
-  apply Nat.max_lub_r with (n := a) (m := product l) (p := product (a::l)) ; exact (product_le l (a)).
+  apply Nat.max_lub_r with (n := a) (m := product l) (p := product (a::l)) ; exact (product_max l (a)).
 Qed.
 
 
@@ -101,12 +99,12 @@ Qed.
 Theorem exists_N_not_in_l : forall l : list nat, exists N : nat, ~In N l.
 Proof.
   intro l.
-  (* set (N := S (sum l)). *)
-  (* set (N := S (product l)). *)
+  (* set (N := S (sum l)). exists N. intro.
+  assert (absurd := sum_le l N H). *)
+  (* set (N := S (product l)). exists N. intro.
+  assert (absurd := product_le l N H). *)
   set (N := S (maximum l)). exists N. intro.
-  (* assert (absurd := le_sum l N H). *)
-  (* assert (absurd := le_prod l N H). *)
-  assert (absurd := le_max l N H).
+  assert (absurd := maximum_le l N H).
   unfold N in absurd. omega.
 Qed.
 
@@ -135,7 +133,4 @@ Corollary infinitely_many_natural_numbers_proof_2 : ~(exists l : list nat, foral
 Proof.
   exact (forall_not_exists (list nat) nat (In (A := nat)) exists_N_not_in_l).
 Qed.
-
-
-
 
